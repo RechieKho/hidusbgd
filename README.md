f92bdc536de641a34db5e2bfdded46b289115efa
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2025-10-13 12:06:53 +0800

    Setup project.

---
+++ README.md
@@ -0,0 +1,3 @@
+# HIDUSBGD
+
+HIDUSBGD is a GDExtension that interfaces with Human Interface Device through USB.
---
+++ code/register_types.hpp
@@ -0,0 +1,11 @@
+#ifndef HIDUSBGD_REGISTER_TYPES_HPP
+#define HIDUSBGD_REGISTER_TYPES_HPP
+
+#include <godot_cpp/core/class_db.hpp>
+
+using namespace godot;
+
+void initialize_hidusbgd_module(ModuleInitializationLevel p_level);
+void uninitialize_hidusbgd_module(ModuleInitializationLevel p_level);
+
+#endif // HIDUSBGD_REGISTER_TYPES_HPP
