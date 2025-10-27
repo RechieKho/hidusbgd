ecac5a03eda01125265d2ad509c82090e3b49673
Rechie Kho <50512341+RechieKho@users.noreply.github.com>
2025-10-27 16:31:29 +0800

    Refactor sample code for read data.

--- sample/scenes/main/main.gd
+++ sample/scenes/main/main.gd
@@ -4,8 +4,10 @@ func _ready() -> void:
 	for overview in HIDUSB.get_device_overviews():
 		printt(overview.product_string, overview.vendor_id, overview.product_id)
 	#var device := HIDUSB.open(0xfba5, 0x1000)
-	#var result = (device.get_input_report(0, 9))
+	#var result := (device.get_input_report(0, 8))
+	#var report_id := result[0]
 	#var data := result.slice(1)
+	#print(report_id)
 	#print(data)
 	#print(data.to_int64_array())
 	
